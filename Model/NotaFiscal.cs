a2c35f2e6e64db78b5004c7372824219ba770ee2
João Gabriel <joao.almeida.gabriel2015@gmail.com>
2023-09-21 15:19:45 -0300

    feat: create Item model.

--- Model/NotaFiscal.cs
+++ Model/NotaFiscal.cs
@@ -7,11 +7,12 @@
         public DateTime date { get; set; }
         public double value { get; set; }
 
-        public NotaFiscal(int id, string emitter, DateTime date)
+        public NotaFiscal(int id, string emitter, DateTime date, double value = 0.0)
         {
             this.ID = id;
             this.emitter = emitter;
             this.date = date;
+            this.value = value;
         }
     }
 }
