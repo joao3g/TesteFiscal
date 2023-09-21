397f60954efbce41bec19968661d25271d0a767c
João Gabriel <joao.almeida.gabriel2015@gmail.com>
2023-09-21 11:35:00 -0300

    feat: NotaFiscal controller was created.

---
+++ Controller/NotaFiscal.cs
@@ -0,0 +1,29 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fiscal.Model;
+
+namespace Fiscal.Controller
+{
+    internal class NotaFiscalController
+    {
+        private const string TABLE_NAME = "NotaFiscal";
+
+        public static async Task Insert(NotaFiscal item, NpgsqlConnection connection)
+        {
+            string commandText = $"INSERT INTO {TABLE_NAME} (idnota, emissor, date, value) VALUES (@id, @emitter, @date, @value)";
+            await using (var cmd = new NpgsqlCommand(commandText, connection))
+            {
+                cmd.Parameters.AddWithValue("id", item.ID);
+                cmd.Parameters.AddWithValue("emitter", item.emitter);
+                cmd.Parameters.AddWithValue("date", item.date);
+                cmd.Parameters.AddWithValue("value", 10.10);
+
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+    }
+}
