cb8d60cb7cae5c4aa23bcebca3060c7c9cdb2459
João Gabriel <joao.almeida.gabriel2015@gmail.com>
2023-09-21 15:19:27 -0300

    feat: create Item controller.

---
+++ Controller/Item.cs
@@ -0,0 +1,41 @@
+﻿using Fiscal.Model;
+using Npgsql;
+
+namespace Fiscal.Controller
+{
+    internal class ItemController
+    {
+        private const string TABLE_NAME = "Item";
+
+        public static async Task Insert(Item item, NpgsqlConnection connection)
+        {
+            string commandText = $"INSERT INTO {TABLE_NAME} (idnota, produto, valor) VALUES (@IDinvoice, @product, @value)";
+            await using (var cmd = new NpgsqlCommand(commandText, connection))
+            {
+                cmd.Parameters.AddWithValue("IDinvoice", item.IDinvoice);
+                cmd.Parameters.AddWithValue("product", item.product);
+                cmd.Parameters.AddWithValue("value", item.value);
+
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+
+        public static async Task<double> getTotalValueByIDinvoice(int IDinvoice, NpgsqlConnection connection)
+        {
+            double result = 0;
+
+            string commandText = $"SELECT SUM(valor) as soma FROM {TABLE_NAME}";
+            await using (var cmd = new NpgsqlCommand(commandText, connection))
+            {
+                await using (NpgsqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    while (await reader.ReadAsync())
+                    {
+                        double? sum = reader["soma"] as double?;
+                        result = sum.Value;
+                    }
+            }
+
+            return result;
+        }
+    }
+}
--- Controller/NotaFiscal.cs
+++ Controller/NotaFiscal.cs
@@ -23,17 +23,18 @@ namespace Fiscal.Controller
         {
             List<NotaFiscal> result = new List<NotaFiscal>();
 
-            string commandText = $"SELECT * FROM {TABLE_NAME}";
+            string commandText = $"SELECT {TABLE_NAME}.idnota as idnota, {TABLE_NAME}.emissor as emissor, {TABLE_NAME}.data as data, SUM(item.valor) as soma FROM {TABLE_NAME} FULL JOIN item ON {TABLE_NAME}.idnota = item.idnota GROUP BY {TABLE_NAME}.idnota";
             await using (var cmd = new NpgsqlCommand(commandText, connection))
             {
                 await using (NpgsqlDataReader reader = await cmd.ExecuteReaderAsync())
                     while (await reader.ReadAsync())
                     {
                         string emitter = reader["emissor"] as string;
-                        int? id = reader["idnota"] as int?;
+                        int? id = reader["idnota"] as int? ?? 0;
                         DateTime? date = reader["data"] as DateTime?;
+                        float? sum = reader["soma"] as float? ?? 1;
 
-                        NotaFiscal nota = new NotaFiscal(id.Value, emitter, date.Value);
+                        NotaFiscal nota = new NotaFiscal(id.Value, emitter, date.Value, sum.Value);
                         result.Add(nota);
                     }
             }
