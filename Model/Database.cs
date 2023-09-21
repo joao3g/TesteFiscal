788d740cca8a2ae292656e384b69b3946e4d0213
João Gabriel <joao.almeida.gabriel2015@gmail.com>
2023-09-21 11:32:52 -0300

    feat: NotaFiscal model was created.

---
+++ Model/Database.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Fiscal.Model
+{
+    internal class Database
+    {
+        public static Database _instance;
+        public NpgsqlConnection connection;
+
+        private static string Host = "localhost";
+        private static string User = "postgres";
+        private static string DBname = "Fiscal";
+        private static string Password = "root";
+        private static string Port = "5432";
+
+        public static Database Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new Database();
+                    _instance.connect();
+                }
+
+                return _instance;
+            }
+        }
+
+        public async void connect()
+        {
+
+            string connString =
+                    string.Format(
+                        "Server={0};Username={1};Database={2};Port={3};Password={4};SSLMode=Prefer",
+                        Host,
+                        User,
+                        DBname,
+                        Port,
+                        Password);
+
+            await using var dataSource = NpgsqlDataSource.Create(connString);
+            await using var connection = await dataSource.OpenConnectionAsync();
+
+            this.connection = connection;
+        }
+    }
+}
---
+++ Model/NotaFiscal.cs
@@ -0,0 +1,25 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiscal.Model
+{
+    internal class NotaFiscal
+    {
+        public int ID { get; set; }
+        public string emitter { get; set; }
+        public DateTime date { get; set; }
+        public double value { get; set; }
+
+        public NotaFiscal(int id, string emitter, DateTime date, double value)
+        {
+            this.ID = id;
+            this.emitter = emitter;
+            this.date = date;
+            this.value = value;
+        }
+    }
+}
